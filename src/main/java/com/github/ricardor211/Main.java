ee389303cb0250fe1448e51751a154900d79180a
Ricardo <65051149+RicardoR211@users.noreply.github.com>
2026-05-21 01:16:40 -0300

    Adiciando Main.java simples

--- src/main/java/com/github/ricardor211/Main.java
+++ src/main/java/com/github/ricardor211/Main.java
@@ -1,17 +1,20 @@
 package com.github.ricardor211;
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+import com.github.ricardor211.questao1.Carro;
+import com.github.ricardor211.questao2.Televisao;
+
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+        // Testando o Carro
+        Carro meuCarro = new Carro("Civic", 5);
+        meuCarro.ligar();
+        meuCarro.acelerar();
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
-        }
+        System.out.println("-----------------");
+
+        // Testando a TV
+        Televisao minhaTv = new Televisao("Samsung", 50.0f, 220);
+        minhaTv.ligar();
+        minhaTv.aumentarVolume();
     }
 }
